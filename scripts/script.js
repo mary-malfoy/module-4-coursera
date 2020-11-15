6b0f9de2c41c388018a3f4818bda87a0e37845e1
mary-malfoy <190317022@stu.sdu.edu.kz>
2020-11-15 23:16:06 +0500

    last commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,30 @@
-console.log("Hello world")
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+(function (window) {
+    var byeSpeaker = {};
+    var speakWord = "Good Bye";
+    byeSpeaker.speak = function (name) {
+      console.log(speakWord + ", " + name+"!");
+    }
+    window.byeSpeaker = byeSpeaker;
+    
+    })(window);
+
+(function (window) {
+    var helloSpeaker = {};
+    var speakWord = "Hello";
+    helloSpeaker.speak = function (name) {
+      console.log(speakWord + ", " + name +"!");
+    }
+    window.helloSpeaker = helloSpeaker;        
+    })(window);
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+
+}
